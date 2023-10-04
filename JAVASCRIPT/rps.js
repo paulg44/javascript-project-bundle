346bba2f7a198941601935d73eb3fc77a6ba084b
paulg44 <paulgarton1984@hotmail.com>
2023-10-04 21:35:14 +0100

    start css, turned it into a horror(saw) rps game

--- JAVASCRIPT/rps.js
+++ JAVASCRIPT/rps.js
@@ -3,18 +3,29 @@ const userInput = document.getElementById("user");
 const userWelcome = document.querySelector("h3");
 const showResult = document.querySelector(".show-result");
 const showScore = document.querySelector(".score");
+const showQuote = document.querySelector(".quote");
 // Button Variables
 const paperBtn = document.getElementById("paper");
 const rockBtn = document.getElementById("rock");
 const scissorsBtn = document.getElementById("scissors");
+
+// Hide buttons initially
+paperBtn.style.visibility = "hidden";
+rockBtn.style.visibility = "hidden";
+scissorsBtn.style.visibility = "hidden";
 
 // Store user input in a variable
 userInput.addEventListener("keypress", function (e) {
   if (e.key === "Enter") {
     let inputValue = e.target.value;
 
+    // Show buttons
+    paperBtn.style.visibility = "visible";
+    rockBtn.style.visibility = "visible";
+    scissorsBtn.style.visibility = "visible";
+
     //   Add text content to h3
-    userWelcome.textContent = `Welcome ${inputValue}, make your choice`;
+    userWelcome.textContent = `Hello ${inputValue}, I want to play a game`;
   }
 });
 
@@ -41,6 +52,9 @@ let score = 0;
 // Variable for result of game
 let result;
 
+// Variable for quotes
+let quote;
+
 // Game Logic
 function gameLogic(userChoice) {
   // Array of possible choice
@@ -61,16 +75,38 @@ function gameLogic(userChoice) {
   } else if (userChoice === computerChoice) {
     result = "Tie!";
   } else {
-    result = "Computer wins!";
+    result = "Jigsaw wins!";
     score -= 1;
   }
 
   // Show results and score
-  showResult.textContent = `You chose ${userChoice}, the computer chose ${computerChoice}. Therefore ${result}`;
+  showResult.textContent = `You chose ${userChoice}, Jigsaw chose ${computerChoice}. Therefore ${result}`;
   showScore.textContent = `Your current score: ${score}`;
+
+  // Quotes dependent on score
+  let plusScoreQuotes = ["great", "excellent", "good"];
+  let minusScoreQuotes = ["bad", "worse", "awful"];
+  // Random quote picker
+  const plusQuote =
+    plusScoreQuotes[Math.floor(Math.random() * plusScoreQuotes.length)];
+  const minusQuote =
+    minusScoreQuotes[Math.floor(Math.random() * minusScoreQuotes.length)];
+
+  if (score > 0) {
+    quote = plusQuote;
+  } else if (score < 0) {
+    quote = minusQuote;
+  } else {
+    quote = "Choose wisely";
+  }
+
+  showQuote.textContent = quote;
 
   // Show reset button on game start
   if (score !== 0 && restartBtn.style.display === "none") {
     restartBtn.style.display = "block";
+    restartBtn.style.position = "absolute";
+    restartBtn.style.bottom = "10px";
+    restartBtn.style.right = "10px";
   }
 }
--- README.md
+++ README.md
@@ -1,9 +1,14 @@
 # javascript-project-bundle
 
-A vanilla JavaScript project, all your favourites, from basic games to to-do lists!
+A vanilla JavaScript and CSS project, all your favourites, from basic games to to-do lists!
 
-I have, for some time, been concentrating more on frameworks (React, Ruby, Typescript) and CSS frameworks (Bootstrap) and I would like to go back a little bit and create a project purely in vanilla JavaScript and CSS. It will consist of all the basic applications people build, maybe with a twist on a few of them, I will try to add API’s where possible. Basically this will be one project with a bunch of applications in it.
+I have, for some time, been concentrating more on frameworks (React, Ruby, Typescript) and CSS frameworks (Bootstrap) and I would like to go back a little bit and create a project purely in vanilla JavaScript and CSS. It will consist of all the basic applications people build, maybe with a twist on a few of them, I will try to add API’s where possible. Basically this will be one project with a bunch of applications in it. I will also try to show mmy design and vanilla CSS skills with some (hopefully) different and fun versions of all these beginner favourites.
 
 ## Rock, Paper, Scissors
 
-Well, this was far more difficult than I anticipated. It just shows if you don't keep on top of everything and don't code a specific way for a while it's so easy to forget basic functions. However I've managed to get the game functioning as I'd expect it to, with a little help form past projects I'm not ashamed to admit.
+Well, this was far more difficult than I anticipated. It just shows if you don't keep on top of everything and don't code a specific way for a while it's so easy to forget basic functions. However I've managed to get the game functioning as I'd expect it to, with a little help form past projects I'm not ashamed to admit. I've added the score logic so the score now keeps up, I have also added a restart game button that displays upon starting the game.
+
+# Takeaways
+
+Javascript - I learned (again) about scopes within function, sometimes you cannot access the things you want as they are function scoped or globally. Also refreshed my memory about textContent, create and amending elements.
+CSS -
