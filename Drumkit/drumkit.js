0cd6eb9a0fd6b85c15780df4de820cb85a9ce41e
paulg44 <paulgarton1984@hotmail.com>
2023-10-07 21:13:23 +0100

    add key stroke cowbell plays

--- Drumkit/drumkit.js
+++ Drumkit/drumkit.js
@@ -1,6 +1,14 @@
 const cowbellAudio = document.getElementById("cowbell");
 const cowbellBtn = document.querySelector(".cowbell");
 
+// Drum audio button clicks
 cowbellBtn.addEventListener("click", () => {
   cowbellAudio.play();
+});
+
+// Drum audio key stroke
+document.addEventListener("keydown", (e) => {
+  if (e.key === "y") {
+    cowbellAudio.play();
+  }
 });
