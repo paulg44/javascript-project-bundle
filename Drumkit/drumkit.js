2ac5030fb602b0f07dc4052aabc21f79c660f9ee
paulg44 <paulgarton1984@hotmail.com>
2023-10-08 09:38:27 +0100

    add audio files and variables

--- Drumkit/drumkit.js
+++ Drumkit/drumkit.js
@@ -1,4 +1,17 @@
+// Audio Variables
+const snareAudio = document.getElementById("snare");
+const hiHatOpenAudio = document.getElementById("hi-hat-open");
+const hiHatClosedAudio = document.getElementById("hit-hat-closed");
+const bassAudio = document.getElementById("bass");
+const tomLowAudio = document.getElementById("tom-low");
+const tomMidAudio = document.getElementById("tom-mid");
+const tomHighAudio = document.getElementById("tom-high");
+const crashAudio = document.getElementById("crash");
+const splashAudio = document.getElementById("splash");
 const cowbellAudio = document.getElementById("cowbell");
+const sticksAudio = document.getElementById("stick");
+
+// Button Variables
 const cowbellBtn = document.querySelector(".cowbell");
 
 // Drum audio button clicks
