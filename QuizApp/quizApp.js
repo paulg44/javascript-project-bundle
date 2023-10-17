a06543c090cdf788c0afeeddc6c67ab08eeb3cfa
paulg44 <paulgarton1984@hotmail.com>
2023-10-17 20:30:27 +0100

    fetch quiz data from url

--- QuizApp/quizApp.js
+++ QuizApp/quizApp.js
@@ -1,1 +1,18 @@
 // JavaScript file for quiz app
+
+// Variable from HTML
+const generateBtn = document.querySelector(".generate");
+
+// API for quiz
+async function generateQuiz() {
+  const response = await fetch(
+    "https://opentdb.com/api.php?amount=15&category=21"
+  );
+  const data = await response.json();
+  return data;
+}
+generateBtn.addEventListener("click", async () => {
+  console.log("btn clicked");
+  const quizData = await generateQuiz();
+  console.log(quizData);
+});
