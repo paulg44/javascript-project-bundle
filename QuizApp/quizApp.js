9691515f4b314034fd209bd0245bfd506be6e757
paulg44 <paulgarton1984@hotmail.com>
2023-10-17 20:53:12 +0100

    added somme text content to page when generate button pressed

--- QuizApp/quizApp.js
+++ QuizApp/quizApp.js
@@ -1,18 +1,32 @@
 // JavaScript file for quiz app
 
+// Parse wrong answers and correct answer and add into an array of all answers
+
 // Variable from HTML
 const generateBtn = document.querySelector(".generate");
+const question = document.querySelector(".question");
+const options = document.querySelector(".options");
+const nextBtn = document.querySelector(".next");
 
 // API for quiz
 async function generateQuiz() {
   const response = await fetch(
-    "https://opentdb.com/api.php?amount=15&category=21"
+    "https://opentdb.com/api.php?amount=10&category=21&type=multiple"
   );
   const data = await response.json();
   return data;
 }
+// Array that has all answers in
+let allAnswersArray = [];
+
+// Generate quiz data on button click
 generateBtn.addEventListener("click", async () => {
-  console.log("btn clicked");
   const quizData = await generateQuiz();
   console.log(quizData);
+  question.textContent = quizData.results[0].question;
+
+  allAnswersArray.push(quizData.results[0].incorrect_answers);
+  allAnswersArray.push(quizData.results[0].correct_answer);
+
+  options.textContent = allAnswersArray;
 });
