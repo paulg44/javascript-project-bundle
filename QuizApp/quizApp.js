6486f73a76d2a227d9a2404f6c3337c276105573
paulg44 <paulgarton1984@hotmail.com>
2023-10-18 20:05:17 +0100

    add questions to checkbox labels and shuffle questions

--- QuizApp/quizApp.js
+++ QuizApp/quizApp.js
@@ -1,11 +1,17 @@
 // JavaScript file for quiz app
 
 // Parse wrong answers and correct answer and add into an array of all answers
+// Turn array answers into checkbox for each answer
+// Assign each checkbox an answer and mix answer using Math random?
 
 // Variable from HTML
 const generateBtn = document.querySelector(".generate");
 const question = document.querySelector(".question");
 const options = document.querySelector(".options");
+const answerOne = document.querySelector(".answer-one");
+const answerTwo = document.querySelector(".answer-two");
+const answerThree = document.querySelector(".answer-three");
+const answerFour = document.querySelector(".answer-four");
 const nextBtn = document.querySelector(".next");
 
 // API for quiz
@@ -25,8 +31,19 @@ generateBtn.addEventListener("click", async () => {
   console.log(quizData);
   question.textContent = quizData.results[0].question;
 
-  allAnswersArray.push(quizData.results[0].incorrect_answers);
-  allAnswersArray.push(quizData.results[0].correct_answer);
+  // Combine all answers
+  allAnswersArray = [
+    ...quizData.results[0].incorrect_answers,
+    quizData.results[0].correct_answer,
+  ];
 
   options.textContent = allAnswersArray;
+  // This shuffles the answers randomly
+  allAnswersArray.sort(() => Math.random() - 0.5);
+
+  // Add an answer to a label
+  answerOne.textContent = allAnswersArray.pop();
+  answerTwo.textContent = allAnswersArray.pop();
+  answerThree.textContent = allAnswersArray.pop();
+  answerFour.textContent = allAnswersArray.pop();
 });
