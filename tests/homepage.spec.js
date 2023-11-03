885cef6b0933bb42aca14b75c93fdc49ad5acacb
paulg44 <paulgarton1984@hotmail.com>
2023-11-03 17:39:09 +0000

    add link test to homepage

--- tests/homepage.spec.js
+++ tests/homepage.spec.js
@@ -2,9 +2,23 @@
 // @ts-check
 const { test, expect } = require("@playwright/test");
 
-test("has title", async ({ page }) => {
+test.beforeEach(async ({ page }) => {
   await page.goto("https://paulg44.github.io/javascript-project-bundle/");
+});
 
-  //   Expect title to be
+// Basic has title test
+test("has title", async ({ page }) => {
   await expect(page).toHaveTitle("Homepage");
+});
+
+// Check a link on project works
+test("link to project", async ({ page }) => {
+  // Arrange
+  //   Used the locator method as all the links have the same role and name
+  const linkBtn = page.locator('[href="./SawRPS/index.html"]');
+
+  //   Act
+  await linkBtn.click();
+  // Assert
+  await expect(page).toHaveTitle("Vanilla JavaScript Bundle of Projects");
 });
