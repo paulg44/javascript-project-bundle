dee20770cc81387a8bddb6f93a5f0f83e98043d2
paulg44 <paulgarton1984@hotmail.com>
2024-02-17 13:21:03 +0000

    add functionality to delete an item fromm the list

--- ToDo/todo.js
+++ ToDo/todo.js
@@ -2,13 +2,23 @@ const input = document.getElementById("todo");
 const submitBtn = document.querySelector(".submit-btn");
 const list = document.querySelector(".todo-list");
 
-// Add Task
-submitBtn.addEventListener("click", () => {
+// Add To List
+function addToList() {
   const inputValue = input.value.trim();
+  const listItem = document.createElement("li");
   if (inputValue !== "") {
-    let listItem = document.createElement("li");
     listItem.textContent = inputValue;
     list.appendChild(listItem);
+    let deleteBtn = document.createElement("button");
+    deleteBtn.innerHTML = "X";
+    deleteBtn.addEventListener("click", deleteFromList);
+    listItem.appendChild(deleteBtn);
   }
   input.value = "";
-});
+}
+
+// Delete from List
+function deleteFromList(e) {
+  const listItem = e.target.parentElement;
+  list.removeChild(listItem);
+}
