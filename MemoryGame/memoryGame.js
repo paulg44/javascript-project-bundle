82361ba3cfae631b4009be26e2eb59329d6cfa0c
paulg44 <paulgarton1984@hotmail.com>
2024-03-02 21:46:47 +0000

    can now compare two chosen square values

--- MemoryGame/memoryGame.js
+++ MemoryGame/memoryGame.js
@@ -28,6 +28,8 @@ function gameLogic(array) {
   let randomGenerator = array.sort(() => 0.5 - Math.random());
   console.log(randomGenerator);
 
+  let squares = [];
+
   //   Render a div for each item
   array.forEach((item) => {
     const square = document.createElement("div");
@@ -37,24 +39,41 @@ function gameLogic(array) {
     square.style.border = "2px solid red";
     gameContainer.appendChild(square);
 
-    // Make squares clickable
-    let clickedSquares = [];
+    squares.push(square);
+  });
+
+  // Make squares clickable
+  let clickedSquares = [];
+
+  squares.forEach((square, index) => {
     square.addEventListener("click", () => {
-      if (square) {
-        console.log("Square clicked");
+      if ((clickedSquares.length, 2 && !clickedSquares.includes(squares))) {
+        console.log(`Square at index ${index} clicked`);
         square.style.backgroundColor =
           square.style.backgroundColor === "red" ? "white" : "red";
         let userChoiceNumber = square.textContent;
         console.log(userChoiceNumber);
+
+        clickedSquares.push(square);
+
+        if (clickedSquares.length === 2) {
+          let firstValue = clickedSquares[0].textContent;
+          let secondValue = clickedSquares[1].textContent;
+
+          if (firstValue === secondValue) {
+            console.log("Values Match");
+            testScore++;
+            console.log(testScore);
+          } else {
+            console.log("Values don't match");
+          }
+
+          clickedSquares = [];
+        }
       }
     });
-
-    // if (userChoiceNumber === userChoiceNumber) {
-    //   testScore++;
-    // }
   });
 }
-console.log(testScore);
 
 // Event Listeners
 easyBtn.addEventListener("click", () => {
